5b08c9394d4876090d93e6caa33fa3483365a220
Mike Morris <michael.patrick.morris@gmail.com>
2014-05-05 15:32:36 -0400

    add more methods to guarded_map

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ template <class K, class V, class Compare = std::less<K>,
 class guarded_map {
   private:
     typedef std::map<K, V, Compare, Allocator> map_type;
+    typedef std::lock_guard<std::mutex> lock_guard;
 
     map_type map;
     std::mutex mutex;
@@ -18,19 +19,39 @@ class guarded_map {
   public:
     typedef typename map_type::const_iterator const_iterator;
 
+    bool empty() {
+      lock_guard guard(this->mutex);
+      return this->map.empty();
+    }
+
+    const_iterator cbegin() {
+      lock_guard guard(this->mutex);
+      return this->map.cbegin();
+    }
+
+    const_iterator cend() {
+      lock_guard guard(this->mutex);
+      return this->map.cend();
+    }
+
     std::pair<const_iterator, bool> emplace(K key, V value) {
-      std::lock_guard<std::mutex> lock(this->mutex);
+      lock_guard guard(this->mutex);
       return this->map.emplace(key, value);
     }
 
     const_iterator find(K key) {
-      std::lock_guard<std::mutex> lock(this->mutex);
+      lock_guard guard(this->mutex);
       return this->map.find(key);
     }
 
-    bool empty() {
-      std::lock_guard<std::mutex> lock(this->mutex);
-      return this->map.empty();
+    std::pair<const_iterator, bool> insert(K key, V value) {
+      lock_guard guard(this->mutex);
+      return this->map.emplace(key, value);
+    }
+
+    size_t size() {
+      lock_guard guard(this->mutex);
+      return this->map.size();
     }
 };
 
